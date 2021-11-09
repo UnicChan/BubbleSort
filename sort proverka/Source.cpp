5acd86a921cf285458f4d6366e844b8a2f7055fc
UnicChan <42190000+UnicChan@users.noreply.github.com>
2021-11-10 01:02:19 +0300

    .

--- sort proverka/Source.cpp
+++ sort proverka/Source.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 
 void change(int a[], int size) {
-	int k = 0;
+	int count = 0;
 	int i = 0;
 	int w = size - 1;
 	int j;
 
-	while (k < w) {
+	while (count < w) {
 		if (i < w) {
 			if (a[i] > a[i + 1]) {
 				j = a[i];
 				a[i] = a[i + 1];
 				a[i + 1] = j;
 				i++;
-				k = 0;
+				count = 0;
 			}
 			else {
 				i++;
-				k++;
+				count++;
 			}
 		}
 		else {
-			k = 0;
+			count = 0;
 			i = 0;
 		}
 	}
